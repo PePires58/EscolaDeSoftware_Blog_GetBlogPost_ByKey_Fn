bc06bb3c5090714fbf3e5436c4fa2f0d31c1ed36
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2022-07-11 20:24:59 -0300

    add verifications into dynamodb service

--- src/services/dynamodbService.js
+++ src/services/dynamodbService.js
@@ -1,14 +1,19 @@
-const { DynamoDBClient, GetItemCommand  } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
 
-exports.GetBlogPostByKey = async function(key){
-    const client = new DynamoDBClient({region: process.env.Region});
-    const command = new GetItemCommand({
-        TableName: process.env.BlogPostTableName,
-        Key: {
-            'title': {S: key}
-        }
-    });
-    const response = await client.send(command);
+exports.GetBlogPostByKey = async function (key) {
+    if (!key) {
+        throw new Error('key is required');
+    }
+    else {
+        const client = new DynamoDBClient({ region: process.env.Region });
+        const command = new GetItemCommand({
+            TableName: process.env.BlogPostTableName,
+            Key: {
+                'title': { S: key }
+            }
+        });
+        const response = await client.send(command);
 
-    return response.Item;
+        return response.Item;
+    }
 }
